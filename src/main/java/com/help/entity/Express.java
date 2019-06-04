bf2e6dac24f312517f1b0d19a2edda8029512a40
zjm <1685813197@qq.com>
2019-06-04 17:49:09 +0800

    add express

---
+++ src/main/java/com/help/entity/Express.java
@@ -0,0 +1,19 @@
+package com.help.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Express {
+    private int expressId;
+    private String address;
+    private double pay;
+    private String phone;
+    private String description;
+    private Timestamp time;
+}
