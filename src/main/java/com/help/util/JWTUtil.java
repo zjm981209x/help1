7076fe36e2526bfa70f1afd8e8dad4f625e9967c
zjm <1685813197@qq.com>
2019-06-06 08:29:28 +0800

    add

--- src/main/java/com/help/util/JWTUtil.java
+++ src/main/java/com/help/util/JWTUtil.java
@@ -7,7 +7,7 @@ import java.util.Date;
 
 public class JWTUtil {
     public static String getToken(String name){
-        Date date = new Date(System.currentTimeMillis() + 15*60*1000);
+        Date date = new Date(System.currentTimeMillis() + 60*60*1000);
         String token = Jwts.builder()
                 .claim("username",name)
                 .signWith(SignatureAlgorithm.HS256,"myjwtsecret")
